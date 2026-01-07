6d0eb0840bf25cddc6319cee9758f4d03255c02b
yuki2005 <yuki20050927@gmail.com>
2026-01-07 20:18:55 +0900

    add 'application.properties.example'

--- src/Position/PosAssign.java
+++ src/Position/PosAssign.java
@@ -21,24 +21,29 @@ public class PosAssign {
     ) {
         Map<ShiftTime, Map<Pos, List<Employee>>> result = new HashMap<>();
 
-        // ★ Schedule から休日判定を取得
+        // Schedule から休日判定を取得
         boolean isHoliday = conditions.isHoliday();
 
-        // ★ Strategy はここで一度だけ決定
+        // 選定ロジックをisHolidayによって決定する
         AssignmentStrategy strategy = strategyFactory.get(isHoliday);
-
+        
+        // 時間帯ごとに選定ロジックを実行する
         for (ShiftTime targetTime : ShiftTime.values()) {
-
+        	
+        	// 時間帯ごとの選ばれた従業員を格納する
             List<Employee> staffForTime =
                     staffByTime.getOrDefault(targetTime, Collections.emptyList());
-
+            
+            // 誰も選ばれなかったら処理を飛ばして次の時間帯に移る
             if (staffForTime.isEmpty()) {
                 continue;
             }
-
+            
+            // 選定された従業員をポジションに割り振る
             Map<Pos, List<Employee>> posAssignments =
                     strategy.assign(staffForTime, conditions, targetTime);
-
+            
+            // 各時間帯の選定された従業員リストを格納したMapに新たに決定された従業員リストを追加する
             result.put(targetTime, posAssignments);
         }
 
